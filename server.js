8f6a2089f20bceb1f220bb27711444038ac0e641
Malgorzata Kot <malgorzata.kot@jobcloud.ch>
2020-08-07 20:35:34 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+// Setup empty JS object to act as endpoint for all routes
+
+projectData = {};
+
+// Express to run server and routes
+
+const express = require("express");
+
+// Start up an instance of app
+
+const app = express();
+
+/* Dependencies */
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/* Middleware*/
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static("website"));
+
+const port = 3000;
+
+// Spin up the server
+// Callback to debug
+
+const server = app.listen(port, () => {
+  console.log(`Server running on: ${port}`);
+});
+
+// Initialize all route with a callback function
+app.get("/all", sendData);
+
+// Callback function to complete GET '/all'
+function sendData(request, response) {
+  response.send(projectData);
+}
+
+// Post Route
+
+app.post("/add", callBack);
+
+function callBack(req, res) {
+  res.send("POST received");
+}
---
+++ website/app.js
@@ -0,0 +1,16 @@
+// Personal API Key for OpenWeatherMap API
+const fsLibrary = require("fs");
+
+const APIKey = fsLibrary.readFileSync("APIkey.txt", "UTF-8");
+
+// Event listener to add function to existing HTML DOM element
+const submitButton = document.getElementById("generate");
+submitButton.addEventListener("click", function () {});
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+/* Function to GET Project Data */
